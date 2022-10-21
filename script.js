a75069b3666753fe7bdf6f516763d7f001694631
luizarn <luizadriven@gmail.com>
2022-10-21 02:00:00 -0300

    versão com distribuição de cartas

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
-function virar(elemento){
-    elemento.classList.toggle("verso")
+const gifs = [
+'angelparrot',
+'angelparrot',
+'autonomousparrot',
+'autonomousparrot',
+'dabparrot',
+'dabparrot',
+'hypnoparrot',
+'hypnoparrot',
+'mergeconflictparrot',
+'mergeconflictparrot',
+'wineparrot',
+'wineparrot',
+'zombieparrot',
+'zombieparrot',
+];
+
+
+window.onload = function iniciar(){
+       let qtdeCartas = Number(prompt('Diga a quantidade de cartas'));
+       console.log(qtdeCartas)
+       while(qtdeCartas%2==1 || qtdeCartas<4 || qtdeCartas >14){
+        alert ("Quantidade de cartas inválida!");
+        qtdeCartas = prompt('Diga a quantidade de cartas');
+       }
+
+     
+        //preciso inserir cartas no containeir que não tem nada, tem que selecionar ele
+        const jogoreal = document.querySelector(".containerjogo");
+        gifs.length = qtdeCartas;
+        gifs.sort(comparador);
+        console.log(jogoreal)
+           //a partir da condição que tem que adicionar até no máximo a qtd de cartas escolhida
+           for(let i = 0; i < qtdeCartas; i++){
+           // e com isso, adicionar a qtde de cartas que foi digitada no container que não tem nada
+          jogoreal.innerHTML += 
+           `  <li onclick="virar(this)" class="card">
+           <div class="frente"></div>
+           <div class="versogif"><img src="./arquivos/${gifs[i]}.gif"></div> 
+           
+       </li>`;
+
+       
+       
+           }
+        }
+
+
+function comparador() {
+    return Math.random() - 0.5;
 }
+
+function virar(elemento){
+    elemento.classList.toggle("versogif");
+}
+        
+
+
+
+    
